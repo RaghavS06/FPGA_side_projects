e703be48dec031582d0d87547a25f31e6142784d
Raghav Sharma <145406356+RaghavS06@users.noreply.github.com>
2026-08-16 14:34:51 -0700

    Rename sync_fifo to async_fifo and update logic

--- fifo/async_fifo/async_fifo.sv
+++ fifo/async_fifo/async_fifo.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/15/2026 01:48:41 PM
+// Create Date: 08/16/2026 01:36:43 PM
 // Design Name: 
-// Module Name: sync_fifo
+// Module Name: async_fifo
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,52 +20,71 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sync_fifo #(parameter DEPTH = 8, parameter WIDTH = 8)(
-input logic clk,
+module async_fifo #(parameter WIDTH = 8, parameter DEPTH = 8)(
+input logic clk_wr,
+input logic clk_re,
 input logic rst,
 input logic wr_en,
 input logic re_en,
-input logic [WIDTH - 1: 0] data_in,
-output logic [WIDTH - 1 : 0] data_out,
+input logic [WIDTH-1:0] data_in,
+output logic [WIDTH-1:0] data_out,
+output logic valid_re,
 output logic full,
-output logic empty,
-output logic re_valid
-    );
-    
-//infer BRAM used in FIFO
-logic [WIDTH - 1 : 0] mem [DEPTH - 1 : 0];
-//determine pointer width for re/wr pointers
-localparam pnt_width = $clog2(DEPTH) + 1;
-//initialize pointers
-logic [pnt_width - 1 : 0] re_pnt, wr_pnt;
-//empty/full flags
-assign empty = re_pnt == wr_pnt;
-assign full = (re_pnt[pnt_width - 2 : 0] == wr_pnt[pnt_width - 2 : 0]) 
-&& (re_pnt[pnt_width - 1] != wr_pnt[pnt_width - 1]);
+output logic empty
+);
 
-//write logic
-always_ff @(posedge clk) begin
-    if (rst) begin
-        wr_pnt <= 0;    
-    end else if (wr_en && !full) begin
-        mem[wr_pnt[pnt_width - 2 : 0]] <= data_in;
-        wr_pnt <= wr_pnt + 1;
-    end
+//infer bram
+logic [WIDTH-1:0] mem [DEPTH-1:0];
+
+//get pointer width and initialize all the pointers im gonna use
+localparam pnt_width = $clog2(DEPTH) + 1;
+logic [pnt_width-1:0] re_pnt, wr_pnt, gray_re_pnt,
+gray_wr_pnt, gray_wr_pnt_1, gray_wr_pnt_2, gray_re_pnt_1, gray_re_pnt_2;
+
+//gray-code conversion from bianry to gray code
+assign gray_re_pnt = re_pnt ^ (re_pnt >> 1);
+assign gray_wr_pnt = wr_pnt ^ (wr_pnt >> 1);
+
+//logic for empty/full flags
+assign empty = gray_re_pnt == gray_wr_pnt_2;
+assign full = (gray_re_pnt_2[pnt_width-1] != gray_wr_pnt[pnt_width-1]) &&
+(gray_re_pnt_2[pnt_width-2] != gray_wr_pnt[pnt_width-2]) &&
+(gray_re_pnt_2[pnt_width-3:0] == gray_wr_pnt[pnt_width-3:0]);
+
+//double flopping both gray coded pointers for stability
+always_ff @(posedge clk_re) begin
+    gray_wr_pnt_1 <= gray_wr_pnt;
+    gray_wr_pnt_2 <= gray_wr_pnt_1;
 end
 
-//read logic
-always_ff @(posedge clk) begin
+always_ff @(posedge clk_wr) begin
+    gray_re_pnt_1 <= gray_re_pnt;
+    gray_re_pnt_2 <= gray_re_pnt_1;
+end
+
+//write code
+always_ff @(posedge clk_wr) begin
+    if (rst) begin
+        wr_pnt <= 0;
+    end else if (wr_en && !full) begin
+        mem[wr_pnt[pnt_width-2:0]] <= data_in;
+        wr_pnt <= wr_pnt + 1;
+    end  
+end
+
+//read code
+always_ff @(posedge clk_re) begin
     if (rst) begin
         re_pnt <= 0;
         data_out <= 0;
-        re_valid <= 0;
+        valid_re <= 0;
     end else if (re_en && !empty) begin
-        data_out <= mem[re_pnt[pnt_width - 2 : 0]];
+        data_out <= mem[re_pnt[pnt_width-2:0]];
         re_pnt <= re_pnt + 1;
-        re_valid <= 1;
+        valid_re <= 1;
     end else begin
-        re_valid <= 0;
+        valid_re <= 0;
     end
-end   
- 
+end
+
 endmodule
